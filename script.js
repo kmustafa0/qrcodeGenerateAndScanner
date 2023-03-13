13a227f0e25218ebd2a0789f82c6e20a0265b8bb
MustafaKole <blackunwer0@gmail.com>
2023-03-13 12:48:57 +0300

    new feature
    inputa keypress eklendi

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ qrInput = wrapper.querySelector('.form input');
 qrImg = wrapper.querySelector('.qr-code img'),
 scanQrCodeBtn = wrapper.querySelector('.scan-btn'),
 btnLink = scanQrCodeBtn.querySelector('a');
-
+// butona basıldığına qr kod oluşturuyor
 generateBtn.addEventListener('click', () => {
     let qrValue = qrInput.value;
     if(!qrValue) return;
@@ -15,7 +15,18 @@ generateBtn.addEventListener('click', () => {
         generateBtn.innerText = "Generate QR Code";
     });
 })
-
+// inputa her veri girildiğinde qr oluşturuyor
+qrInput.addEventListener('keypress', () => {
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generateBtn.innerText = "Generating QR Code..."
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`
+    qrImg.addEventListener('load', () => {
+        wrapper.classList.add('active');
+        generateBtn.innerText = "Generate QR Code";
+    });
+})
+// inputta veri kalmadığında formu küçültüyor && qr'ı gizliyor
 qrInput.addEventListener('keyup', () => {
     if(!qrInput.value) {
         wrapper.classList.remove("active");
